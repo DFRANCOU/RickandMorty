e1f363920f03bb5515eb81e1f24c6290fdd24d68
DAVID FRANCO <davidf.francou@gmail.com>
2022-09-07 21:15:44 -0500

    funtion button

--- APIR&M.js
+++ APIR&M.js
@@ -1,7 +1,6 @@
 window.addEventListener("DOMContentLoaded", getCharacters)
 
-function getCharacters(gender, status) {
-  
+function getCharacters(gender, status,) {
     const results = fetch(
         `https://rickandmortyapi.com/api/character?gender=${gender || ""}&status=${status || ""}`);
     results
@@ -36,32 +35,21 @@ function getCharacters(gender, status) {
 const params = new URLSearchParams(window.location.search)
 getCharacters(params.get("gender"), params.get("status"));
 
-// const buttonP = document.getElementsByClassName("prev")
-// const buttonN = document.getElementsByClassName("next")
+const buttonP = document.querySelector(".prev")
+const buttonN = document.querySelector(".next")
 
-// buttonP.addEventListener("click", getCharacters)
+buttonP.addEventListener("click", pagination)
+buttonN.addEventListener("click", pagination)
 
 
-// getCharacters(data => {
-//     data.results.forEach(characters => {
+function pagination (done) {
+  
+  const info = fetch(
+    "https://rickandmortyapi.com/api/character/?page=2")
+  info
+        .then(response => response.json())
+        .then(data => { 
+           done(data)})
+}
 
-//       const article = document.createRange().createContextualFragment(/*html*/`
-//       <article> 
-//         <div class="cards">
-//           <h2>${characters.name}</h2>
-//           <div class="charaters">
-//            <img src ="${characters.image}" class="charatersIma">
-//           </div>
-//          <span id="Gender">${characters.gender}</span>
-//          <span id="aboutChar">${characters.species}</span>
-//         </div>  
-//       </article> 
-//       `);
-
-//       const main = document.querySelector("main");
-      
-//       main.append(article);
-
-//    });
-
-// });
+console.log(pagination)
