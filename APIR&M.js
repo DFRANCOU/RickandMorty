1cd087786d323fd2d3bb56668fb91b56e515da73
DFRANCOU <davidf.frncou@gmail.com>
2022-09-19 16:20:02 -0500

    bottuns3

--- APIR&M.js
+++ APIR&M.js
@@ -1,108 +1,129 @@
 
 
-const RanMapi = 'https://rickandmortyapi.com/api/character/';
+// const RandMapi = 'https://rickandmortyapi.com/api/character/';
 
-getCharacters = (RanMapiURL) => {
-          fetch(RanMapiURL)
-          .then(res => res.json())
-          .then(data => {
-             characters(data),
-             pagination(data.info)
-          })
+// getCharacters = (RandMapiURL) => {
+//           fetch(RandMapiURL)
+//           .then(res => res.json())
+//           .then(data => {
+//              characters(data),
+//              pagination(data.info)
+//           })
               
   
-      .catch(error => { console.error('error: ', error)})
-}
-
-const characters = (data) => {
-
-        data.results.forEach(characters => {
-        const article = document.createRange().createContextualFragment(/*html*/`
-            <article> 
-              <div class="cards">
-                <h2>${characters.name}</h2>
-                <div class="charaters">
-                  <img src ="${characters.image}" class="charatersIma">
-                </div>
-                <span id="Gender">${characters.gender}</span>
-                <span id="aboutChar">${characters.species}</span>
-              </div>  
-            </article> 
-            `);
-                  
-            const main = document.querySelector("main");
-                        
-            main.append(article);
-      });
-       
-}              
-
-const pagination = (info) => {
-  let html = `<li><a href="" onclick="getCharacters('${info.prev}')"> previous </a></li>`
-  html += `<li><a href=""onclick="getCharacters('${info.next}')"> next </a></li>`
-  document.getElementById("pagination").innerHTML = html
-}
-
-getCharacters(RanMapi);
-
-
-
-// function getCharacters(gender, status,) {
-//     const results = fetch(
-//         `https://rickandmortyapi.com/api/character?gender=${gender || ""}&status=${status || ""}`);
-
-//     results
-//           .then(response => response.json())
-//           .then(data => { 
-//             //  done(data)
-            
-//             data.results.forEach(characters => {
-
-//                 const article = document.createRange().createContextualFragment(/*html*/`
-//                 <article> 
-//                   <div class="cards">
-//                     <h2>${characters.name}</h2>
-//                     <div class="charaters">
-//                      <img src ="${characters.image}" class="charatersIma">
-//                     </div>
-//                    <span id="Gender">${characters.gender}</span>
-//                    <span id="aboutChar">${characters.species}</span>
-//                   </div>  
-//                 </article> 
-//                 `);
-          
-//                 const main = document.querySelector("main");
-                
-//                 main.append(article);
-          
-//              });
-//           });
-      
+//       .catch(error => { console.error('error: ', error)})
 // }
+
+// const characters = (data) => {
+
+//         data.results.forEach(characters => {
+//         const article = document.createRange().createContextualFragment(/*html*/`
+//             <article> 
+//               <div class="cards">
+//                 <h2>${characters.name}</h2>
+//                 <div class="charaters">
+//                   <img src ="${characters.image}" class="charatersIma">
+//                 </div>
+//                 <span id="Gender">${characters.gender}</span>
+//                 <span id="aboutChar">${characters.species}</span>
+//               </div>  
+//             </article> 
+//             `);
+                  
+//             const main = document.querySelector("main");
+                        
+//             main.append(article);
+//       });
+       
+// }              
 
 // const params = new URLSearchParams(window.location.search);
 // getCharacters(params.get("gender"), params.get("status"));
 
-// function pag(done){
-//   const info = fetch(
-//     `https://rickandmortyapi.com/api/character/?page=1`);
-//   info
-//      .then(response = response.json())
-//      .then(data => {
-//        done(data)
+// const pagination = (info)
 
-//    data.info.forEach(pagination => {
-//      const div = document.createRange().createContextualFragment(/*html*/`
-//      <div>
-//        <button class="buttonPn" class="prev">${pagination.next} < </button>
-//        <button class="buttonPn" class="next">${pagination.prev} > </button>
-//      </div>
-//      `);
 
-//      const pag = document.querySelector(".next_prev");
 
-//      pag.append(div);
+// const button = ( pagination ) {
+//   for (var i = 0; i = < 42; i++)
+// }
 
-//    });
-// });
-// }  
+
+
+// const pagination = (info) => { 
+//   let html = `<li><a href="" onclick="getCharacters('${info.prev}')"> previous </a></li>`
+//   html += `<li><a href=""onclick="getCharacters('${info.next}')"> next </a></li>`
+//   document.getElementById("pagination").innerHTML = html
+// }
+ 
+// getCharacters(RandMapi);
+
+// const params = new URLSearchParams(window.location.search);
+// getCharacters(params.get("gender"), params.get("status"));
+
+
+
+function getCharacters(gender, status, prev, next) {
+    const results = fetch(
+        `https://rickandmortyapi.com/api/character?gender=${gender || ""}&status=${status || ""}&prev=${prev || ""}&next=${next || ""}`);
+
+    results
+          .then(response => response.json())
+          .then(data => { 
+            //  done(data)
+                    
+            data.results.forEach(characters => {
+              
+                const article = document.createRange().createContextualFragment(/*html*/`
+                <article> 
+                  <div class="cards">
+                    <h2>${characters.name}</h2>
+                    <div class="charaters">
+                     <img src ="${characters.image}" class="charatersIma">
+                    </div>
+                   <span id="Gender">${characters.gender}</span>
+                   <span id="aboutChar">${characters.species}</span>
+                  </div>  
+                </article> 
+                `);
+          
+                const main = document.querySelector("main");
+                
+                main.append(article);
+          
+             });
+
+             data.info.forEach(pagination => {
+
+              const pag = document.createRange().createContextualFragment(`
+                    <button id="prev" class="buttonPag" onclick="getCharacters('${pagination.prev}')"> << </button>  
+                    <button id="next" class="buttonPag" onclick="getCharacters('${pagination.next}')"> >> </button>`);
+
+                    const page = document.querySelector(".buttons");
+                
+                    main.append(article);
+             })
+            
+            //  const buttonPrev = document.getElementById("prev");
+            //  const buttonNext = document.getElementById("next");
+
+            //  buttonPrev.addEventListener("onclick", getCharacters);
+            //  buttonNext.addEventListener("onclick", getCharacters);
+
+          
+          });
+      
+}
+
+const params = new URLSearchParams(window.location.search);
+getCharacters(params.get("gender"), params.get("status"), params.get("next"), params.get("prev"));
+
+
+
+
+
+
+//  const pagination = (info) => {}
+ 
+
+// // https://www.youtube.com/watch?v=S8YV9YfK9nI&t=617s
