0f5b7036148df81a88c5c06bf72ffc5e4a6bf96f
DAVID FRANCO <davidf.francou@gmail.com>
2022-09-22 21:26:23 -0500

    final prev

--- APIR&M.js
+++ APIR&M.js
@@ -1,9 +1,8 @@
+function getData(gender, status,) {
+  const results = fetch(
+      `https://rickandmortyapi.com/api/character?gender=${gender || ""}&status=${status || ""}`);
 
-
-
-
-<<<<<<< HEAD
-    results
+  results
           .then(response => response.json())
           .then(data => { 
             //  done(data)
@@ -22,33 +21,6 @@
                   </div>  
                 </article> 
                 `);
-=======
-
-// function getCharacters(gender, status, prev, next) {
-//     const results = fetch(
-//         `https://rickandmortyapi.com/api/character?gender=${gender || ""}&status=${status || ""}`);
-
-//     results
-//           .then(response => response.json())
-//           .then(data => { 
-//             //  done(data)
-            
-           
-//               data.results.forEach(characters => {
-              
-//                 const article = document.createRange().createContextualFragment(/*html*/`
-//                 <article> 
-//                   <div class="cards">
-//                     <h2>${characters.name}</h2>
-//                     <div class="charaters">
-//                      <img src ="${characters.image}" class="charatersIma">
-//                     </div>
-//                    <span id="Gender">${characters.gender}</span>
-//                    <span id="aboutChar">${characters.species}</span>
-//                   </div>  
-//                 </article> 
-//                 `);
->>>>>>> 29dae9f97aa2b1407465e5e1fcc1030589d2d4bd
           
 //                 const main = document.querySelector("main");
                 
@@ -71,7 +43,6 @@
 // const params = new URLSearchParams(window.location.search);
 // getCharacters(params.get("gender"), params.get("status"));
 
-<<<<<<< HEAD
 
 
 
@@ -84,13 +55,6 @@ function pag(done){
      .then(response = response.json())
      .then(data => {
        done(data)
-=======
-// const pagination = (info) => { 
-//   let html = `<li><a href="" onclick="getCharacters('${info.prev}')"> previous </a></li>`
-//   html += `<li><a href=""onclick="getCharacters('${info.next}')"> next </a></li>`
-//   document.getElementById("pagination").innerHTML = html
-// }
->>>>>>> 29dae9f97aa2b1407465e5e1fcc1030589d2d4bd
 
 
  
